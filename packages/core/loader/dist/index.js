a6a5d50d869c15e57179145a0a78d952416de23d
lvyuanxin <1498781418@qq.com>
2023-02-16 09:27:45 +0800

    feat: plugin

--- packages/core/loader/dist/index.js
+++ packages/core/loader/dist/index.js
@@ -7,29 +7,32 @@ var InjectColumnName = "_vc-col";
 var InjectPathName = "_vc-path";
 var InjectNodeName = "_vc-node";
 
-// src/index.ts
-module.exports = function(content) {
-  function injectDom(domAst, filePath2, source) {
-    if (domAst.type !== 1) {
-      return source;
-    }
-    if (domAst.children && domAst.children.length) {
-      for (let i = domAst.children.length - 1; i >= 0; i--) {
-        const node = domAst.children[i];
-        source = injectDom(node, filePath2, source);
-      }
-    }
-    const codeLines = source.split("\n");
-    const line = domAst.loc.start.line;
-    const column = domAst.loc.start.column;
-    const columnToInject = column + domAst.tag.length;
-    const targetLine = codeLines[line - 1];
-    const nodeName = domAst.tag;
-    const newLine = targetLine.slice(0, columnToInject) + ` ${InjectLineName}="${line}" ${InjectColumnName}="${column}" ${InjectPathName}="${filePath2}" ${InjectNodeName}="${nodeName}"` + targetLine.slice(columnToInject);
-    codeLines[line - 1] = newLine;
-    source = codeLines.join("\n");
+// src/utils.ts
+function injectDom(domAst, filePath, source) {
+  if (domAst.type !== 1) {
     return source;
   }
+  if (domAst.children && domAst.children.length) {
+    for (let i = domAst.children.length - 1; i >= 0; i--) {
+      const node = domAst.children[i];
+      source = injectDom(node, filePath, source);
+    }
+  }
+  const codeLines = source.split("\n");
+  const line = domAst.loc.start.line;
+  const column = domAst.loc.start.column;
+  const columnToInject = column + domAst.tag.length;
+  const targetLine = codeLines[line - 1];
+  const nodeName = domAst.tag;
+  const newLine = targetLine.slice(0, columnToInject) + ` ${InjectLineName}="${line}" ${InjectColumnName}="${column}" ${InjectPathName}="${filePath}" ${InjectNodeName}="${nodeName}"` + targetLine.slice(columnToInject);
+  codeLines[line - 1] = newLine;
+  source = codeLines.join("\n");
+  return source;
+}
+var utils_default = injectDom;
+
+// src/index.ts
+module.exports = function(content) {
   const filePath = this.resourcePath;
   const params = new URLSearchParams(this.resource);
   const type = params.get("type");
@@ -37,7 +40,7 @@ module.exports = function(content) {
     const contentAfterParased = (0, import_compiler_sfc.parse)(content);
     const domAst = contentAfterParased.descriptor.template.ast;
     const originalTemplate = domAst.loc.source;
-    const newTemplate = injectDom(domAst, filePath, originalTemplate);
+    const newTemplate = utils_default(domAst, filePath, originalTemplate);
     return content.replace(originalTemplate, newTemplate);
   } else {
     return content;
--- packages/playground/vue2/vue.config.js
+++ packages/playground/vue2/vue.config.js
@@ -1,8 +1,11 @@
+const InspectorPlugin = require('@webpack-vue-plugin-inspector/plugin')
 const { defineConfig } = require('@vue/cli-service')
-
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new InspectorPlugin()]
+  },
   chainWebpack: config => {
     config.module
       .rule('vue')
@@ -10,5 +13,7 @@ module.exports = defineConfig({
       .use('@webpack-vue-plugin-inspector/loader')
       .loader('@webpack-vue-plugin-inspector/loader')
       .end();
+    // config.plugin('@webpack-vue-plugin-inspector/plugin')
+    //   .use(new InspectorPlugin())
   }
 })
