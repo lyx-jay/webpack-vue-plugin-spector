3eeccb3b3934045bd43ae65f52406415347083c9
lvyuanxin <1498781418@qq.com>
2023-02-23 20:04:02 +0800

    feat: support webpack4

--- src/core/devServer.ts
+++ src/core/devServer.ts
@@ -38,11 +38,22 @@ export const launchEditorMiddleware: RequestHandler = (req, res, next) => {
 
 }
 
-
+/**
+ * vue/cli@5.x.x devServer 配置
+ */
 export const domToCodeDevServerV5 = {
-  // for webpack 5
   setupMiddlewares: (middlewares: any[] = []): any[] => {
     return [launchEditorMiddleware, ...middlewares]
+  },
+}
+
+/**
+ * vue/cli@4.x.x devServer 配置
+ */
+export const domToCodeDevServerV4 = {
+  // for webpack 4
+  before: (app: any) => {
+    app.use(launchEditorMiddleware)
   },
 }
 
